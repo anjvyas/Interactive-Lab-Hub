ec58e2c23c9e4c99781db00977bcccb752787bf8
Anjali Vyas <anjalivyas@dhcp-vl2052-820.redrover.cornell.edu>
2022-11-29 15:55:27 -0500

    changed both interfaces

--- Final_Project/clinician_interface/main.py
+++ Final_Project/clinician_interface/main.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template, request
 
-import paho.mqtt.client as mqtt
-import uuid
-import json
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
-# Every client needs a random ID
-client = mqtt.Client(str(uuid.uuid1()))
-# configure network encryption etc
-client.tls_set()
-# this is the username and pw we have setup for the class
-client.username_pw_set('idd', 'device@theFarm')
+load_dotenv()
 
-#connect to the broker
-client.connect(
-    'farlab.infosci.cornell.edu',
-    port=8883)
-
-topic = 'IDD/pill_dispenser'
+# Database connection + fetching function
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   db_pwd = os.getenv("DB_PWD")
+   CONNECTION_STRING = f"mongodb+srv://anjali:{db_pwd}@cluster0.sea1mts.mongodb.net/?retryWrites=true&w=majority"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['pill_dispenser']
 
 app = Flask(__name__)
 
@@ -29,9 +30,22 @@ def form():
     if request.method == 'GET':
         return render_template('doctor.html')
     if request.method == 'POST':
-        json_data = {}
         doctor_data = request.form
+        
+        pilldispenser_db = get_database()
+        pres_collection = pilldispenser_db['prescriptions']
+        
+        # add the prescription information to our DB!
+        pres_info = pres_collection.find_one({"id": 1})
+
+        to_add = {"id": 1}
+
         for k, v in doctor_data.items():
-            json_data[k] = v
-        client.publish(topic, json.dumps(json_data))
+            to_add[k] = v.split(",")
+
+        if pres_info != None:
+            pres_collection.replace_one({"id": 1}, to_add)
+        else:
+            pres_collection.insert_one(to_add)
+
         return render_template('doctor.html')
---
+++ Final_Project/older_adult_interface/send_reminder.py
@@ -0,0 +1,56 @@
+import time
+import datetime
+import calendar
+import os
+import json
+
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Database connection + fetching function
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   db_pwd = os.getenv("DB_PWD")
+   CONNECTION_STRING = f"mongodb+srv://anjali:{db_pwd}@cluster0.sea1mts.mongodb.net/?retryWrites=true&w=majority"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['pill_dispenser']
+
+def findDay():
+    born = datetime.datetime.strptime(str(datetime.date.today()), '%Y-%m-%d').weekday()
+    return (calendar.day_name[born])
+
+while True:
+    # get the current day and time
+    currentDay = findDay()
+    current_time = datetime.datetime.now()
+
+    # get prescription info from our DB
+    pilldispenser_db = get_database()
+    pres_collection = pilldispenser_db['prescriptions']
+    pres_info = pres_collection.find_one({"id": 1})
+
+    if pres_info != None:
+        times = pres_info[currentDay]
+        medicine_name = pres_info["name"]
+        if len(times) > 0:
+            for t in times:
+                time_data = t.split(":")
+                h = time_data[0]
+                m = time_data[1]
+                if str(current_time.hour) == h and str(current_time.minute) == str(m):
+                    # for mac testing
+                    os.system(f"say 'Hi Aashika it is time to take {medicine_name}'")
+                    # # for pi testing
+                    # os.system(f"flite -voice slt -t 'Hi Aashika it is time to take {medicine_name}'")
+                    time.sleep(60)
+    else:
+        os.system(f"flite -voice slt -t 'The prescription has not been loaded yet'")
+
+    time.sleep(1)
